3989842249655d71b3bde15a7a13560439aa2c0c
Mira <mira.theidel@gmx.de>
2023-02-03 14:54:35 +0100

    define Doctor struct and mapping

--- contracts/DocScheduler.sol
+++ contracts/DocScheduler.sol
@@ -4,12 +4,42 @@ pragma solidity ^0.8.0;
 import './Ownable.sol';
 import './Evaluations/Evaluation.sol';
 
-contract DocScheduler is Ownable {
-  //todo lege eine Struktur an die alle wichtigen Daten einer Arztpraxis enthält z.B. Name, öffnungszeiten, WalletAdresse des Arzt
+//https://remix.ethereum.org
 
+contract DocScheduler is Ownable {
+
+  //todo lege eine Struktur an die alle wichtigen Daten einer Arztpraxis enthält z.B. Name, öffnungszeiten, WalletAdresse des Arzt
+  struct Doctor {
+      string first_name;
+      string last_name;
+      address wallet_address; // https://dev.singularitynet.io/docs/concepts/ethereum-address/
+      OfficeDay[] office_hours;
+      Description description; // evtl direkt in html Format oder besser verschiedene description variablen
+      Specialization [] specialization;
+  }
+
+  struct OfficeDay {
+    uint opening_time;
+    uint closing_time;
+    uint start_lunchbreak;
+    uint stop_lunchbreak;
+  }
+
+  struct Description {
+    string street;
+    uint street_number;
+    uint plz;
+    string city;
+    string phone_number;
+    string free_text;
+  }
+
+  enum Specialization { HAUSARZT, ZAHNARZT, HNO-ARZT, ORTHOPAEDE, KARDEOLOGE, AUGENARZT}
+  
   constructor() {}
 
   //todo lege Mapping vom Datentyp address auf die Struktur an damit alle erzeugten Arztpraxen gespeichert werden können
+  mapping(address => Doctor) doctors;
 
   //todo erstelle createDoctorsOffice Methode
   //diese soll aus der Website aufgerufen werden und die Struktur übergeben bekommen
